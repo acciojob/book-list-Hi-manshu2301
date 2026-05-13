b57ade9440fa5905070858dd634bce5b287b89dc
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-05-13 11:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const submit=document.getElementById("submit");
+const bookList = document.getElementById("book-list");
+
+submit.addEventListener("click",function(){
+	const title = document.getElementById("title").value;
+	const author= document.getElementById("author").value;
+	const isbn = document.getElementById("isbn").value;
+	
+	if(title==="" || author==="" || isbn===""){
+		alert("Please fill all the values");
+		return;
+	}
+	const row=document.createElement("tr");
+	row.innerHTML = `
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete">&times;</button></td>`;
+	bookList.appendChild(row);
+
+	document.getElementById("title").value="";
+	document.getElementById("author").value="";
+	document.getElementById("value").value="";
+	
+});
+bookList.addEventListener("click",function(e){
+	if(e.target.classList.contains("delete")){
+		e.target.parentElement.parentElement.remove();
+	}	
+});
+
+
+
+
